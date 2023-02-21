b4d3390842064f1ec26372319909b795f98fffc8
Bogdan Vlad <bogdan@genez.io>
2023-02-21 22:15:53 +0200

    Fix bug in integration tests.

--- test_hello.py
+++ test_hello.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     genezio_login("735614a4514b93b523fd90cd2342d2ed013ccf6ba38b90bea6fcc6c36a23942d775d83bb03c5539441e28fab8bd6c9acdbdde4c64bb3360bf6cfcda891d2b68f")
 
     os.chdir("./projects/hello-world/server/")
-    status, project_url = genezio_deploy(False)
+    status, project_url, _ = genezio_deploy(False)
 
     assert status == 0, "genezio deploy returned non-zero exit code"
     assert project_url != "", "genezio deploy returned empty project url"
