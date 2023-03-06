77135a380a1f1ab110208b6bab43debf966b5fa2
Bogdan Vlad <bogdan@genez.io>
2023-03-06 18:43:11 +0200

    Add informative logs.

--- utils.py
+++ utils.py
@@ -6,4 +6,8 @@ def run_node_script(script, args=[]):
     run_script_command = ['node', script] + args
     process = subprocess.run(run_script_command, capture_output=True, text=True)
 
+    if process.returncode != 0:
+        print(process.stderr)
+        print(process.stdout)
+
     return process.returncode, process.stdout
