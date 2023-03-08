bd10c0bf66fa973cc7e9e64937c6a106858e06e6
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2023-03-08 17:46:32 +0200

    Add frontend deployment test
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>

---
+++ test_frontend.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import os
+from genezio import genezio_deploy, genezio_login
+from utils import run_npm_build
+
+def test_frontend():
+    print("Starting frontend test...")
+    token = os.environ.get('GENEZIO_TOKEN')
+    
+    genezio_login(token)
+
+    os.chdir("./projects/mini-frontend/server/")
+    deploy_result = genezio_deploy(False)
+
+    assert deploy_result.return_code == 0, "genezio deploy returned non-zero exit code"
+    assert deploy_result.project_url != "", "genezio deploy returned empty project url"
+
+    os.chdir("../client/")
+
+    status = run_npm_build()
+    assert status == 0, "`npm run build` returned non-zero exit code"
+
+    os.chdir("../server/")
+    
+    deploy_result = genezio_deploy(True)
+
+    assert deploy_result.return_code == 0, "genezio deploy --frontend returned non-zero exit code"
+    assert deploy_result.project_url != "", "genezio deploy --frontend returned empty project url"
+    assert "https://genezio-test-frontend" in deploy_result.project_url, "genezio deploy --frontend returned wrong project url"
+
+    print("Test passed!")
+
+# Test order matters because the commands are having side effects.
+if __name__ == '__main__':
+    test_frontend()
