dd2fa5346d57eb6cef838681f08b4c38d6414f87
Andreia Ocănoaia <andreia@genez.io>
2023-03-31 11:37:23 +0300

    GNZ-372 Add dart sdk test (#9)
    * Add dart SDK test
    
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
    
    * Add dart SDK test to dev CI/CD
    
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
    
    ---------
    
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>

---
+++ projects/dart-sdk/server/index.ts
@@ -0,0 +1,17 @@
+export class Server {
+
+    method() {
+    }
+
+    methodWithoutParameters(): string {
+        return "string"
+    }
+
+    methodWithOneParameter(test1: string): string {
+        return "string"
+    }
+
+    methodWithMultipleParameters(test1: string, test2: number, test3: boolean): string {
+        return "string"
+    }
+}
