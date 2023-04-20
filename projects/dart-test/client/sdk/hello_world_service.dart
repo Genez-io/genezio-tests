5885455a424a8c2a5dd59c62d28c91a9f3ea1330
Bogdan Vlad <bogdan@genez.io>
2023-04-20 11:43:01 +0300

    Add more test cases to dart test.

--- projects/dart-test/client/sdk/hello_world_service.dart
+++ projects/dart-test/client/sdk/hello_world_service.dart
@@ -59,34 +59,76 @@ class HelloWorldService {
     await remote.call("HelloWorldService.methodWithPointReturnVoid", [point]);
   }
 
+  static Future<String> methodWithReturnSimpleString() async {
+    final response = await remote.call("HelloWorldService.methodWithReturnSimpleString", []);
+
+    return response as String;
+  }
+
+  static Future<int> methodWithReturnSimpleInt() async {
+    final response = await remote.call("HelloWorldService.methodWithReturnSimpleInt", []);
+
+    return response as int;
+  }
+
+  static Future<bool> methodWithReturnSimpleBool() async {
+    final response = await remote.call("HelloWorldService.methodWithReturnSimpleBool", []);
+
+    return response as bool;
+  }
+
+  static Future<List<Point>> replacePointsInArray(List<Point> list) async {
+    final response = await remote.call("HelloWorldService.replacePointsInArray", [list]);
+
+    return (response as List<dynamic>).map((e) => Point.fromJson(e as Map<String, dynamic>),).toList();
+  }
+
+  static Future<Map<String, Point>> replacePointsInMap(Map<String, Point> map) async {
+    final response = await remote.call("HelloWorldService.replacePointsInMap", [map]);
+
+    return (response as Map<String, dynamic>).map((k, e) => MapEntry(k,Point.fromJson(e as Map<String, dynamic>),));
+  }
+
   static Future<List<int>> getNumbers(int x, int y, int z) async {
     final response = await remote.call("HelloWorldService.getNumbers", [x, y, z]);
 
-    return response as List<int>;
+    return (response as List<dynamic>).map((e) => e as int).toList();
   }
 
   static Future<List<String>> getStrings(String x, String y, String z) async {
     final response = await remote.call("HelloWorldService.getStrings", [x, y, z]);
 
-    return response as List<String>;
+    return (response as List<dynamic>).map((e) => e as String).toList();
   }
 
   static Future<List<int>> getNumbersAsFuture(int x, int y, int z) async {
     final response = await remote.call("HelloWorldService.getNumbersAsFuture", [x, y, z]);
 
-    return response as List<int>;
+    return (response as List<dynamic>).map((e) => e as int).toList();
+  }
+
+  static Future<List<Point>> getPoints(int x, int y, int count) async {
+    final response = await remote.call("HelloWorldService.getPoints", [x, y, count]);
+
+    return (response as List<dynamic>).map((e) => Point.fromJson(e as Map<String, dynamic>),).toList();
+  }
+
+  static Future<List<Point>> getPointsAsync(int x, int y, int count) async {
+    final response = await remote.call("HelloWorldService.getPointsAsync", [x, y, count]);
+
+    return (response as List<dynamic>).map((e) => Point.fromJson(e as Map<String, dynamic>),).toList();
   }
 
   static Future<TestResult> getTestResult() async {
     final response = await remote.call("HelloWorldService.getTestResult", []);
 
-    return TestResult.fromJson(response);
+    return TestResult.fromJson(response as Map<String, dynamic>);
   }
 
   static Future<ComplicatedClass> getComplicatedClass(int x, int y) async {
     final response = await remote.call("HelloWorldService.getComplicatedClass", [x, y]);
 
-    return ComplicatedClass.fromJson(response);
+    return ComplicatedClass.fromJson(response as Map<String, dynamic>);
   }
 
 }
