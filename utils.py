edafb2942564ec53bc76e63ad9bc2a4786f25966
Radu Dumitrescu <d.raduandrei@gmail.com>
2023-05-18 20:24:39 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python3
 
 import subprocess
-import platform
-
-def set_shell():
-    if platform.system() == "Windows":
-        return True
-    else:
-        return False
-    
-def cmp_files(path_1, path_2):
-    l1 = l2 = True
-    with open(path_1, 'r') as f1, open(path_2, 'r') as f2:
-        while l1 and l2:
-            l1 = f1.readline()
-            l2 = f2.readline()
-            if l1 != l2:
-                print(l1)
-                print(l2)
-                return False
-    return True
 
 def run_npm_run_build():
-    shell = set_shell()
-    process = subprocess.run(['npm', 'run', 'build'], capture_output=True, text=True, shell=shell)
-
-    return process.returncode, process.stdout
+    process = subprocess.run(['npm', 'run', 'build'], capture_output=True, text=True, shell=True)
 
 def run_curl(url):
     if url == None:
@@ -33,8 +11,7 @@ def run_curl(url):
         return 1
 
     print("Running curl " + url)
-    shell = set_shell()
-    process = subprocess.run(['curl', url], capture_output=True, text=True, shell=shell)
+    process = subprocess.run(['curl', url], capture_output=True, text=True, shell=True)
 
     if process.returncode != 0:
         print(process.stderr)
@@ -44,8 +21,7 @@ def run_curl(url):
 
 def run_node_script(script, args=[]):
     run_script_command = ['node', script] + args
-    shell = set_shell()
-    process = subprocess.run(run_script_command, capture_output=True, text=True, shell=shell)
+    process = subprocess.run(run_script_command, capture_output=True, text=True, shell=True)
 
     if process.returncode != 0:
         print(process.stderr)
@@ -55,7 +31,7 @@ def run_node_script(script, args=[]):
 
 def run_script(args):
     run_script_command = args
-    process = subprocess.run(run_script_command, capture_output=True, text=True)
+    process = subprocess.run(run_script_command, capture_output=True, text=True, shell=True)
 
     if process.returncode != 0:
         print(process.stderr)
