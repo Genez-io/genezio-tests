e4ba2fa603387b0e58ad7cfa715e9aff89fbada0
Costin Sin <56958521+costinsin@users.noreply.github.com>
2023-12-09 12:41:52 +0200

    Fix genezio account message expectation (#54)

--- test_unauthenticated.py
+++ test_unauthenticated.py
@@ -17,7 +17,7 @@ def test_unauthenticated():
     return_code, stderr, stdout = genezio_logout()
 
     assert return_code == 0, "genezio account returned non-zero exit code"
-    assert "You are now logged out!" in stdout, "genezio account returned wrong output"
+    assert "You are now logged out." in stdout, "genezio account returned wrong output"
     
     return_code, stderr, stdout = genezio_account()
     assert return_code == 1, "genezio account returned non-zero exit code"
