ed37049e6ea87d6dfb201a86308388fc4053668e
cristim67 <miloiuc4@gmail.com>
2024-03-12 15:50:55 +0200

    debug windows dart test

--- genezio.py
+++ genezio.py
@@ -21,6 +21,7 @@ class DeployResult:
         self.stderr = stderr
 
         link_regex = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
+        print(stdout)
         links = re.findall(link_regex, str(stdout))
         links = [x for x in links if "genezio.com/docs" not in x[0]]
 
