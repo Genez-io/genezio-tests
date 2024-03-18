3ff915b4bfd2bcfba21ac7290d9df50376f733fe
Bogdan Ciobanu <46264496+MagoDelBlocco@users.noreply.github.com>
2024-03-18 14:26:46 +0200

    PatchResultUndefined

--- projects/binary-dependency/client/test-binary-dependency.js
+++ projects/binary-dependency/client/test-binary-dependency.js
@@ -3,7 +3,7 @@ import { BinaryDependencyTest } from "@genezio-sdk/binary-dependency"
 (async () => {
     const result = await BinaryDependencyTest.test()
     const regexExp = /^[$0-9a-zA-Z.\/]*$/gi;
-    const correct = regexExp.test(result);
+    const correct = regexExp.test(result) && result;
 
     console.log(correct ? "Ok" : "Error")
 })();
