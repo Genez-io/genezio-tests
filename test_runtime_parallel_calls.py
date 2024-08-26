bf1f2c87703df11580ca31c86395aea6d73a274d
Tudor Pipernea <T.Pipernea@student.tudelft.nl>
2024-08-26 14:46:16 +0200

    Refactored the code

--- test_runtime_parallel_calls.py
+++ test_runtime_parallel_calls.py
@@ -9,72 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-base_url = os.getenv("BASE_FUNCTION_URL")
-def test_parallel_calls():
-    print("Starting parallel calls test...")
-    token = os.environ.get('GENEZIO_TOKEN')
-    genezio_login(token)
-    os.chdir("./projects/express-test-template/")
-    deploy_result = genezio_deploy(False) 
-    base_url = deploy_result.stdout_all_links[0][0] + "/hello/";
-    assert deploy_result.return_code == 0, "genezio deploy returned non-zero exit code"
-    assert deploy_result.project_url != "", "genezio deploy returned empty project url"
-
-    def call_function():
-        def send_request(random_number):
-            
-            url = f"{base_url}{random_number}"
-            response = requests.get(url)
-            print(f"Called URL: {url}, Status Code: {response.status_code}, Response: {response.text}")
-            assert response.status_code == 200, f"Expected status code 200, got {response.status_code}, for URL: {url}"
-            assert response.text == str(random_number), f"Expected response text {random_number}, got {response.text}, for URL: {url}"
-
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = []
-            for _ in range(10):
-                random_number = random.randint(1, 100) * 2
-                futures.append(executor.submit(send_request, random_number))
-            concurrent.futures.wait(futures)
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    future.result()
-                except AssertionError as e:
-                    print(f"Assertion failed: {e}")
-                    raise 
-                except Exception as e:
-                    print(f"An error occurred: {e}")
-                    raise    
-
-    def call_function_with_fails():
-        def send_request(random_number):
-            url = f"{base_url}{random_number}"
-            print(random_number)
-            response = requests.get(url)
-            print(f"Called URL: {url}, Status Code: {response.status_code}, Response: {response.text}")
-            if random_number % 2 == 1:
-                assert response.status_code == 404, f"Expected status code 404, got {response.status_code}, for URL: {url}"
-            else:
-                assert response.status_code == 200, f"Expected status code 200, got {response.status_code}, for URL: {url}"
-                assert response.text == str(random_number), f"Expected response text {random_number}, got {response.text}, for URL: {url}"
-            time.sleep(0.01)
-
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = []
-            for _ in range(10):
-                random_number = random.randint(1, 100)
-                futures.append(executor.submit(send_request, random_number))
-            concurrent.futures.wait(futures)
-
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    future.result()
-                except AssertionError as e:
-                    print(f"Assertion failed: {e}")
-                    raise 
-                except Exception as e:
-                    print(f"An error occurred: {e}")
-                    raise  
-    def call_function_with_timeouts():
+def call_function_with_timeouts(base_url):
         def send_request(random_number):
 
             url = f"{base_url}timeout/{random_number}"
@@ -101,58 +36,122 @@ def test_parallel_calls():
                     raise 
                 except Exception as e:
                     print(f"An error occurred: {e}") 
-                    raise   
+                    raise 
 
-    def call_function_with_some_js_errors():
-        def send_request(random_number):
+def call_function(base_url):
+    def send_request(random_number):
+        url = f"{base_url}{random_number}"
+        response = requests.get(url)
+        print(f"Called URL: {url}, Status Code: {response.status_code}, Response: {response.text}")
+        assert response.status_code == 200, f"Expected status code 200, got {response.status_code}, for URL: {url}"
+        assert response.text == str(random_number), f"Expected response text {random_number}, got {response.text}, for URL: {url}"
 
-            url = f"{base_url}error/{random_number}"
-            print(random_number)
-            response = requests.get(url)
-            print(f"Called URL: {url}, Status Code: {response.status_code}, Response: {response.text}")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for _ in range(10):
+            random_number = random.randint(1, 100) * 2
+            futures.append(executor.submit(send_request, random_number))
+        concurrent.futures.wait(futures)
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()
+            except AssertionError as e:
+                print(f"Assertion failed: {e}")
+                raise 
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                raise  
 
-            if random_number % 3 != 0:
-                assert response.status_code == 200, f"Expected status code 200, got {response.status_code}, for URL: {url}"
-                assert response.text == str(random_number), f"Expected response text {random_number}, got {response.text}, for URL: {url}"
-            else:
-                assert response.status_code != 200, f"Expected status code not 200, got {response.status_code}, for URL: {url}"
-            time.sleep(0.01)
+def call_function_with_some_js_errors(base_url):
+    def send_request(random_number):
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = []
-            for _ in range(10):
-                random_number = random.randint(1, 100)
-                futures.append(executor.submit(send_request, random_number))
-            concurrent.futures.wait(futures) 
+        url = f"{base_url}error/{random_number}"
+        print(random_number)
+        response = requests.get(url)
+        print(f"Called URL: {url}, Status Code: {response.status_code}, Response: {response.text}")
+
+        if random_number % 3 != 0:
+            assert response.status_code == 200, f"Expected status code 200, got {response.status_code}, for URL: {url}"
+            assert response.text == str(random_number), f"Expected response text {random_number}, got {response.text}, for URL: {url}"
+        else:
+            assert response.status_code != 200, f"Expected status code not 200, got {response.status_code}, for URL: {url}"
+        time.sleep(0.01)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for _ in range(10):
+            random_number = random.randint(1, 100)
+            futures.append(executor.submit(send_request, random_number))
+        concurrent.futures.wait(futures) 
+
+    for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()
+            except AssertionError as e:
+                raise 
+            except Exception as e:
+                print(f"An error occurred: {e}") 
+                raise      
+
+def run_functions_in_parallel(base_url):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(call_function_with_timeouts, base_url),
+            executor.submit(call_function_with_some_js_errors, base_url)
+        ]
+        
+        for future in concurrent.futures.as_completed(futures):
+            if future.exception() is not None:
+                raise future.exception()  
+
+def call_function_with_fails(base_url):
+    def send_request(random_number):
+        url = f"{base_url}{random_number}"
+        print(random_number)
+        response = requests.get(url)
+        print(f"Called URL: {url}, Status Code: {response.status_code}, Response: {response.text}")
+        if random_number % 2 == 1:
+            assert response.status_code == 404, f"Expected status code 404, got {response.status_code}, for URL: {url}"
+        else:
+            assert response.status_code == 200, f"Expected status code 200, got {response.status_code}, for URL: {url}"
+            assert response.text == str(random_number), f"Expected response text {random_number}, got {response.text}, for URL: {url}"
+        time.sleep(0.01)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for _ in range(10):
+            random_number = random.randint(1, 100)
+            futures.append(executor.submit(send_request, random_number))
+        concurrent.futures.wait(futures)
 
         for future in concurrent.futures.as_completed(futures):
-                try:
-                    future.result()
-                except AssertionError as e:
-                    raise 
-                except Exception as e:
-                    print(f"An error occurred: {e}") 
-                    raise   
+            try:
+                future.result()
+            except AssertionError as e:
+                print(f"Assertion failed: {e}")
+                raise 
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                raise 
+
+def test_parallel_calls():
+    print("Starting parallel calls test...")
+    token = os.environ.get('GENEZIO_TOKEN')
+    genezio_login(token)
+    os.chdir("./projects/express-parallel-calls-template/")
+    deploy_result = genezio_deploy(False) 
+    base_url = deploy_result.web_urls[0] + "/hello/";
+    assert deploy_result.return_code == 0, "genezio deploy returned non-zero exit code"
+    assert deploy_result.project_url != "", "genezio deploy returned empty project url"  
+
     print("GOOD WHEATHER FUNCTION")
-    call_function()
+    call_function(base_url=base_url)
 
     print("FUNCTION WITH FAILS")
-    call_function_with_fails()
+    call_function_with_fails(base_url=base_url)
 
-    def run_functions_in_parallel():
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [
-                executor.submit(call_function_with_timeouts),
-                executor.submit(call_function_with_some_js_errors)
-            ]
-            
-            for future in concurrent.futures.as_completed(futures):
-                if future.exception() is not None:
-                    raise future.exception()
-
-         
-
-    run_functions_in_parallel();
+    
+    run_functions_in_parallel(base_url=base_url);
     os.chdir("../")
 
     print("Prepared to delete project...")
