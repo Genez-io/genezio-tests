0e44c4e481cdb4840d8af20c82539a6b1a738b98
Tudor Pipernea <T.Pipernea@student.tudelft.nl>
2024-08-26 12:23:57 +0200

    deleted console logs

--- projects/express-test-template/.express/init/welcome.js
+++ projects/express-test-template/.express/init/welcome.js
@@ -1,8 +1,1 @@
 import { PORT } from "./server.js";
-
-console.log("");
-// log node-env
-console.log("- NODE_ENV=" + process.env.NODE_ENV);
-// log port
-console.log("- PORT=" + PORT);
-console.log("");
