4df163d050a1179b2d6b3e6a59572180bec72f14
Cristi Miloiu <miloiuc4@gmail.com>
2025-01-20 16:55:15 +0200

    fix

--- projects/host-check-nextjs/cypress.config.ts
+++ projects/host-check-nextjs/cypress.config.ts
@@ -4,10 +4,17 @@ import process from "process";
 export default defineConfig({
   e2e: {
     experimentalRunAllSpecs: true,
+    baseUrl: process.env.NEXT_URL || 'http://localhost:3000',
     setupNodeEvents(on, config) {
     },
+    video: false,
+    screenshotOnRunFailure: true,
   },
   env: {
     url: process.env.NEXT_URL,
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0
   }
 });
